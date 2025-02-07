1969975a7e57951469f9d451c869d0ba866ae9fe
Striker2783 <owenxingli@gmail.com>
2025-02-06 20:28:06 -0500

    Candidates Work

--- crates/apriori/src/apriori.rs
+++ crates/apriori/src/apriori.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use datasets::transaction_set::TransactionSet;
 
-use crate::array2d::Array2D;
+use crate::{array2d::Array2D, hash_tree::AprioriHashTree};
 #[derive(Debug)]
 pub struct Apriori<'a> {
     data: &'a TransactionSet,
@@ -38,6 +38,80 @@ impl Candidates {
     fn run(&mut self, data: &Apriori) {
         self.run_one(data);
         self.run_two(data);
+        while !self.candidates.last().unwrap().is_empty() {
+            let c_prev: Vec<_> = self.candidates.last().unwrap().iter().collect();
+            let mut tree = AprioriHashTree::<50>::default();
+            for i in 0..c_prev.len() {
+                for j in 0..i {
+                    let c1 = c_prev[i];
+                    let c2 = c_prev[j];
+                    if c1[..(c1.len() - 1)] != c2[..(c1.len() - 1)] {
+                        continue;
+                    }
+                    let join = if c1.last().unwrap() > c2.last().unwrap() {
+                        let mut temp = c2.clone();
+                        temp.push(*c1.last().unwrap());
+                        temp
+                    } else {
+                        let mut temp = c1.clone();
+                        temp.push(*c2.last().unwrap());
+                        temp
+                    };
+                    if self.can_be_pruned(&join) {
+                        continue;
+                    }
+                    tree.add(&join);
+                }
+            }
+            if tree.len() == 0 {
+                break;
+            }
+            let k = self.candidates.len() + 1;
+            let mut stack = vec![0; k];
+            for i in 0..data.data.transactions.len() {
+                self.add_to_tree(&mut tree, &data.data.transactions[i], 0, k, &mut stack);
+            }
+            let mut set = HashSet::new();
+            for (arr, n) in tree.iter() {
+                if n >= data.min_support {
+                    set.insert(arr.to_vec());
+                }
+            }
+            self.candidates.push(set);
+        }
+    }
+    fn add_to_tree<const N: usize>(
+        &self,
+        tree: &mut AprioriHashTree<N>,
+        data: &[usize],
+        i: usize,
+        k: usize,
+        stack: &mut [usize],
+    ) {
+        if i == k {
+            let mut v = Vec::new();
+            for i in stack {
+                v.push(data[*i]);
+            }
+            println!("{v:?}");
+            tree.increment(&v);
+            return;
+        }
+        let start = if i == 0 { 0 } else { stack[i - 1] + 1 };
+        for j in start..data.len() {
+            stack[i] = j;
+            self.add_to_tree(tree, data, i + 1, k, stack);
+        }
+    }
+    fn can_be_pruned(&self, v: &[usize]) -> bool {
+        let mut arr: Vec<_> = v.iter().cloned().skip(1).collect();
+        for i in 0..(v.len() - 2) {
+            if !self.candidates[arr.len() - 1].contains(&arr) {
+                return true;
+            }
+            arr[i] = v[i + 1];
+        }
+        false
     }
     fn run_one(&mut self, data: &Apriori) {
         let mut first = vec![0u64; data.data.num_items];
@@ -66,7 +140,7 @@ impl Candidates {
         let mut v = HashSet::new();
         for (r, c, count) in second.iter() {
             if count >= data.min_support {
-                v.insert(vec![r, c]);
+                v.insert(vec![c, r]);
             }
         }
         self.candidates.push(v);
@@ -97,11 +171,14 @@ mod tests {
         );
         let apriori = Apriori::new(&example, 2, 1);
         let candidates = apriori.get_candidates();
-        assert!(candidates.candidates[1].contains(&vec![1, 0]));
-        assert!(candidates.candidates[1].contains(&vec![2, 0]));
-        assert!(candidates.candidates[1].contains(&vec![4, 0]));
-        assert!(candidates.candidates[1].contains(&vec![2, 1]));
-        assert!(candidates.candidates[1].contains(&vec![3, 1]));
-        assert!(candidates.candidates[1].contains(&vec![4, 1]));
+        assert!(candidates.candidates[1].contains(&vec![0, 1]));
+        assert!(candidates.candidates[1].contains(&vec![0, 2]));
+        assert!(candidates.candidates[1].contains(&vec![0, 4]));
+        assert!(candidates.candidates[1].contains(&vec![1, 2]));
+        assert!(candidates.candidates[1].contains(&vec![1, 3]));
+        assert!(candidates.candidates[1].contains(&vec![1, 4]));
+        assert_eq!(candidates.candidates[1].len(), 6);
+        assert_eq!(candidates.candidates.len(), 3);
+        assert_eq!(candidates.candidates[2].len(), 2);
     }
 }
