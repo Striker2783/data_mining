41b0158abfdfd6867209b2003e67b166399bc96e
Striker2783 <owenxingli@gmail.com>
2025-02-14 16:15:41 -0500

    Nested Loop Optimized by a lot.

--- crates/datasets/src/utils.rs
+++ crates/datasets/src/utils.rs
@@ -1,25 +1,21 @@
-pub fn nested_loops<T: FnMut(Vec<usize>)>(mut f: T, data: &[usize], k: usize) {
+pub fn nested_loops<T: FnMut(&[usize])>(mut f: T, data: &[usize], k: usize) {
     let mut stack = vec![0; k];
-    nested_loops_helper(&mut f, data, 0, k, &mut stack);
+    nested_loops_helper(&mut f, data, 0, 0, k, &mut stack);
 }
-fn nested_loops_helper<T: FnMut(Vec<usize>)>(
+fn nested_loops_helper<T: FnMut(&[usize])>(
     f: &mut T,
     data: &[usize],
     i: usize,
+    start: usize,
     k: usize,
     stack: &mut [usize],
 ) {
     if i == k {
-        let mut v = Vec::new();
-        for i in stack {
-            v.push(data[*i]);
-        }
-        f(v);
+        f(&stack);
         return;
     }
-    let start = if i == 0 { 0 } else { stack[i - 1] + 1 };
     for j in start..data.len() {
-        stack[i] = j;
-        nested_loops_helper(f, data, i + 1, k, stack);
+        stack[i] = data[j];
+        nested_loops_helper(f, data, i + 1, j + 1, k, stack);
     }
 }
