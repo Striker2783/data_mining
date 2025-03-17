8e1f6e974b668788bdad5277531464f2c6be19a0
Striker2783 <owenxingli@gmail.com>
2025-03-17 14:17:26 -0400

    Dataset Documentation

--- crates/datasets/src/utils.rs
+++ crates/datasets/src/utils.rs
@@ -1,7 +1,9 @@
+/// A nested loop function that loops through the data, sending an array of k size to the function
 pub fn nested_loops<T: FnMut(&[usize])>(mut f: T, data: &[usize], k: usize) {
     let mut stack = vec![0; k];
     nested_loops_helper(&mut f, data, 0, 0, k, &mut stack);
 }
+/// The recursive helper for nested_loops
 fn nested_loops_helper<T: FnMut(&[usize])>(
     f: &mut T,
     data: &[usize],
@@ -10,16 +12,18 @@ fn nested_loops_helper<T: FnMut(&[usize])>(
     k: usize,
     stack: &mut [usize],
 ) {
+    // If the number of elements on the stack is equal to k, call f
     if i == k {
         f(stack);
         return;
     }
+    // Loop through the rest of the data, add it to the stack, and recursively call
     for j in start..data.len() {
         stack[i] = data[j];
         nested_loops_helper(f, data, i + 1, j + 1, k, stack);
     }
 }
-
+/// Same as nested_loops, but with a prune function
 pub fn nested_loops_prune<T: FnMut(&[usize]), U: Fn(&[usize]) -> bool>(
     mut f: T,
     prune: U,
