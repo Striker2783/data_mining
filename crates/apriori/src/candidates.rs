5b94afef2ac3c6f0201dc8ad4393dcab11f448bf
Striker2783 <owenxingli@gmail.com>
2025-03-18 15:46:16 -0400

    Candidates Documentation

--- crates/apriori/src/candidates.rs
+++ crates/apriori/src/candidates.rs
@@ -2,14 +2,16 @@ use std::{collections::{HashMap, HashSet}, ops::{Deref, DerefMut}};
 
 pub type CandidateType = HashSet<Vec<usize>>;
 pub type CandidateCounter = HashMap<Vec<usize>, u64>;
-
+/// A wrapper for the Candidates
 #[derive(Debug, Default, Clone)]
 pub struct Candidates(CandidateType);
 impl Candidates {
+    /// Constructor
     pub fn new(v: CandidateType) -> Self {
         Self(v)
     }
 }
+// Dereferences to the inner type
 impl Deref for Candidates {
     type Target = CandidateType;
 
--- crates/apriori/src/candidates_func.rs
+++ crates/apriori/src/candidates_func.rs
@@ -1,17 +1,21 @@
 use std::collections::HashMap;
-
+/// Joins together the itemsets for Apriori
 pub fn join<T: FnMut(Vec<usize>)>(v: &[&Vec<usize>], mut f: T) {
+    // A map containing the prefixes and the last elements
     let mut map = HashMap::new();
     for c in v.iter() {
         map.entry(&c[..(c.len() - 1)])
             .and_modify(|v: &mut Vec<usize>| v.push(*c.last().unwrap()))
             .or_insert(vec![*c.last().unwrap()]);
     }
+    // Loops through the map
     for (k, v) in map.into_iter() {
+        // Join together each 2 combinations of v 
         for i in 0..v.len() {
             for j in (i + 1)..v.len() {
                 let c1 = v[i];
                 let c2 = v[j];
+                // Create the join vec
                 let mut join = k.to_vec();
                 if c2 > c1 {
                     join.push(c1);
@@ -20,6 +24,7 @@ pub fn join<T: FnMut(Vec<usize>)>(v: &[&Vec<usize>], mut f: T) {
                     join.push(c2);
                     join.push(c1);
                 }
+                // Call the function
                 f(join);
             }
         }
