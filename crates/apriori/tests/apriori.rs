3880da7acb8a8ce85d18e4c6cfbb5930f746dc23
Striker2783 <owenxingli@gmail.com>
2025-04-04 16:23:55 -0400

    Apriori Test
    Tests

---
+++ crates/apriori/tests/apriori.rs
@@ -0,0 +1,65 @@
+mod common;
+
+use std::collections::HashSet;
+
+use apriori::{
+    apriori::Apriori, apriori_hybrid::AprioriHybrid, apriori_tid::AprioriTID,
+    apriori_trie::AprioriT,
+};
+use common::{Solved, test_generic};
+
+#[test]
+fn test_apriori() {
+    test_generic(|t, s| {
+        let a = Apriori::new(s);
+        let s = a.run(&t);
+        let mut combined = HashSet::new();
+        for c in s.iter() {
+            for e in c.iter() {
+                combined.insert(e.clone());
+            }
+        }
+        Solved::new(combined)
+    });
+}
+
+#[test]
+fn test_tid() {
+    test_generic(|t, s| {
+        let a = AprioriTID::new(s);
+        let s = a.run(&t);
+        let mut combined = HashSet::new();
+        for c in s.iter() {
+            for e in c.iter() {
+                combined.insert(e.clone());
+            }
+        }
+        Solved::new(combined)
+    });
+}
+#[test]
+fn test_hybrid() {
+    test_generic(|t, s| {
+        let a = AprioriHybrid::new(s, 4);
+        let s = a.run(&t);
+        let mut combined = HashSet::new();
+        for c in s.iter() {
+            for e in c.iter() {
+                combined.insert(e.clone());
+            }
+        }
+        Solved::new(combined)
+    });
+}
+#[test]
+fn test_trie() {
+    test_generic(|t, s| {
+        let mut a = AprioriT::new(s);
+        a.run(&t);
+        let mut combined = HashSet::new();
+        a.trie().for_each(s, |e| {
+            combined.insert(e.to_vec());
+        });
+        Solved::new(combined)
+    });
+}
---
+++ crates/count_distribution/tests/apriori.rs
@@ -0,0 +1,36 @@
+mod common;
+
+use std::{collections::HashSet, sync::Arc};
+
+use common::{Solved, test_generic};
+use count_distribution::{count_distribution::CountDistribution, count_distribution_hybrid::CountDistributionHybrid};
+
+#[test]
+fn test_apriori() {
+    test_generic(|t, s| {
+        let a = Arc::new(t);
+        let a = CountDistribution::new(a, 4, s);
+        let s = a.run();
+        let mut combined = HashSet::new();
+        for c in s.iter() {
+            for e in c.iter() {
+                combined.insert(e.clone());
+            }
+        }
+        Solved::new(combined)
+    });
+}
+#[test]
+fn test_apriori_hybrid() {
+    test_generic(|t, s| {
+        let a = CountDistributionHybrid::new(&t, 4, s, 3);
+        let s = a.run();
+        let mut combined = HashSet::new();
+        for c in s.iter() {
+            for e in c.iter() {
+                combined.insert(e.clone());
+            }
+        }
+        Solved::new(combined)
+    });
+}
